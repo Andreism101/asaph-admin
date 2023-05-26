9feb0958e1945b8fb9dd927226da06afeb903912
Andreism101 <don.andrewleck27@gmail.com>
2023-05-27 03:17:28 +0800

    fixed work item 39

--- components/TopCards.jsx
+++ components/TopCards.jsx
@@ -15,12 +15,6 @@ const TopCards = () => {
             <p className='font-semibold text-lg pt-4'>Active Loans</p>
         </div>
       </div>
-      <div className='bg-white flex justify-between w-full p-4 border rounded-md ease-in-out duration-300 hover:bg-blue-500 hover:text-white'>
-        <div className='flex flex-col w-full'>
-            <p className='text-3xl font-bold text-end'>154</p>
-            <p className='font-semibold text-lg pt-4'>Active Users</p>
-        </div>
-      </div>
     </div>
   )
 }
