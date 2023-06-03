028b2d56ea5c8f8e09604e337b6754bc9b75d976
Andreism101 <don.andrewleck27@gmail.com>
2023-06-04 02:15:36 +0800

    fixed footer warning

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='pt-24 m-0'>
-        <footer class="bg-gray-700 shadow">
-            <div class=" items-center md:w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <footer className="bg-gray-700 shadow">
+            <div className=" items-center md:w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                   © 2023 
-                  <a href="/" class="hover:underline">
+                  <a href="/" className="hover:underline">
                     MICLOAN™
                   </a>
                   . All Rights Reserved.
